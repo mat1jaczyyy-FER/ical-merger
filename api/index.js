5640cd6b2b71cbc7c6b72e14f987cd24b9844cab
mat1jaczyyy <mat1jaczyyy@gmail.com>
2022-11-04 11:20:10 +0100

    ferko labos

--- api/index.js
+++ api/index.js
@@ -6,7 +6,10 @@ const ical2json = require('ical2json');
 const url = require('url');
 
 async function filter(req, res, predicate) {
-    var ical = ical2json.convert((await axios.get(`https://www.fer.unizg.hr/_download/calevent/mycal.ics?${url.parse(req.url).query}`)).data);
+    var query = url.parse(req.url).query;
+    var ical = ical2json.convert((await axios.get(`https://www.fer.unizg.hr/_download/calevent/mycal.ics?${query}`)).data);
+    var ferko = ical2json.convert((await axios.get(`http://ferko.fer.hr/ferko/ical/ICalUser.action?${query}`)).data);
+    ical.VCALENDAR[0].VEVENT.push(...ferko.VCALENDAR[0].VEVENT);
     let events = [];
     for (var i of ical.VCALENDAR[0].VEVENT) {
         if (predicate(i)) {
